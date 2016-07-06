afcb13585ceb67cd7eef191c9f1fa08954e7aa74
Neylson Crepalde <neylsoncrepalde@gmail.com>
2016-07-06 00:59:21 -0300

    linha 30 - analisando o Gini

--- scripts/wto_dados_desagregados_analises.R
+++ scripts/wto_dados_desagregados_analises.R
@@ -2,9 +2,9 @@
 # ANALYSIS
 # Neylson Crepalde
 
-#source("C:/Users/neyls/Documents/Neylson Crepalde/Doutorado/GIARS/wto/wto_dados_desagregados.R")
-#source("C:/Users/neyls/Documents/Neylson Crepalde/Doutorado/GIARS/wto/wto_dados_desagregados2.R")
-#source("C:/Users/neyls/Documents/Neylson Crepalde/Doutorado/GIARS/wto/wto_dados_desagregados3.R")
+source("/home/neylson/Documentos/Neylson Crepalde/Doutorado/GIARS/wto/wto_dados_desagregados.R")
+source("/home/neylson/Documentos/Neylson Crepalde/Doutorado/GIARS/wto/wto_dados_desagregados2.R")
+source("/home/neylson/Documentos/Neylson Crepalde/Doutorado/GIARS/wto/wto_dados_desagregados3.R")
 
 library(statnet)
 library(intergraph)
@@ -20,9 +20,26 @@ atributos3[,11] <- 2014
 names(atributos3)[11] <- "Year"
 
 ############################
-# Calculando o GINI e plotando a Lorenz Curve
+# Calculando o Gini e plotando a Lorenz Curve
 ineq(atributos[[10]], type = "Gini")
-plot(Lc(atributos[[10]]), col="darkred", lwd=2)
+plot(Lc(atributos[[10]]), col="darkred", lwd=2,
+     main="Lorenz Curve - World Exports 2010",xlab="Share in world total exports",
+     ylab = "Countries")
+
+
+# Analisando o Gini
+gini11 <- ineq(atributos[[8]], type = "Gini")
+gini12 <- ineq(atributos2[[8]], type = "Gini")
+gini13 <- ineq(atributos3[[8]], type = "Gini")
+gini1 <- c(gini11,gini12,gini13)
+gini21 <- ineq(atributos[[10]], type = "Gini")
+gini22 <- ineq(atributos2[[10]], type = "Gini")
+gini23 <- ineq(atributos3[[10]], type = "Gini")
+gini2 <- c(gini21,gini22,gini23)
+
+ggplot(data=NULL, aes(x=c(2010,2011,2014)))+geom_path(aes(y=gini1), color="darkred")+
+  geom_path(aes(y=gini2),color="blue")+ylim(.8,1)+
+  labs(x="",y="Gini Coefficient",title="World Exports")
 
 #######################
 n.out <- network(as.matrix(get.adjacency(g.out)),directed=T)

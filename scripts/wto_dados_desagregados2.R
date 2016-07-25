14152e9df6c54d9ca5cbc13bc6601bda1c40a337
Neylson Crepalde <neylsoncrepalde@gmail.com>
2016-07-25 12:17:55 -0300

    delete países s/ info (85)

--- scripts/wto_dados_desagregados2.R
+++ scripts/wto_dados_desagregados2.R
@@ -81,9 +81,14 @@ nomes.df.merge2 <- merge(nomes.df2, atributos2, by.x = "name2", by.y = "Country"
 atributos.order2 <- nomes.df.merge2[order(nomes.df.merge2$X1.length.name2.),]
 names(atributos.order2)[2] <- "id"
 atributos.order2$name2 %<>% as.character
-for (i in 2:10) {
-  atributos.order2[[i]] %<>% recode(., .missing=0)
-  }
+
+#Verificando quais países não tem informações
+sem.na <- atributos.order2 %>% na.omit(.) %>% .[[2]]
+excluir <- which(atributos.order2[[2]] %in% sem.na ==F)
+
+#Retirando os países
+atributos.order2 %<>% na.omit
+g.out2 <- delete_vertices(g.out2, excluir)
 
 #Adicionando atributos
 V(g.out2)$populacao <- atributos.order2[[3]]
